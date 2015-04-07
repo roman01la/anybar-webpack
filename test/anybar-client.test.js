2f9a7c6533729290e41d4f3ec1b5d828c8c4f982
Roman Liutikov <roman01la@romanliutikov.com>
2015-04-08 01:19:36 +0300

    add test

---
+++ test/anybar-client.test.js
@@ -0,0 +1,25 @@
+var dgram = require('dgram');
+var anyBarClient = require('../lib/anybar-client');
+
+var cmd = 'test',
+    port = 1799;
+
+var testMsg = 'Should receive command: ' + cmd;
+
+var server = dgram.createSocket('udp4', function (msg) {
+
+    if (msg.toString('utf-8') !== cmd) {
+
+        throw new Error(testMsg);
+    }
+    else {
+
+        console.log(testMsg);
+        server.close();
+        process.exit(0);
+    }
+});
+
+server.bind(port);
+
+anyBarClient(cmd, port);
